24a8114f098afbb11b124f041419fa09c65f8cbe
Borja Vera <borvercas@alum.us.es>
2021-05-24 18:26:11 +0200

    correcciones

--- src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListTest.java
+++ src/test/java/acme/testing/authenticated/task/AuthenticatedTaskListTest.java
@@ -36,27 +36,19 @@ public class AuthenticatedTaskListTest extends AcmePlannerTest {
 	@ParameterizedTest
 	@CsvFileSource(resources = "/authenticated/task/list-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(20)
-	public void listAllNegative(final int recordIndex, final String executionStart, final String executionEnd, final String title, final String isPublic, final String workload, final String link, final String description) {
-		super.signIn("administrator", "administrator");
-		super.clickOnMenu("Authenticated", "List tasks");
-		
-		super.checkColumnHasValue(recordIndex, 0, executionStart);
-		super.checkColumnHasValue(recordIndex, 1, executionEnd);
-		super.checkColumnHasValue(recordIndex, 2, title);
-		
-		super.clickOnListingRecord(recordIndex);
-		
-		super.checkInputBoxHasValue("title", title);
-		super.checkInputBoxHasValue("isPublic", isPublic);
-		super.checkInputBoxHasValue("executionStart", executionStart);
-		super.checkInputBoxHasValue("executionEnd", executionEnd);
-		super.checkInputBoxHasValue("workload", workload);
-		super.checkInputBoxHasValue("link", link);
-		super.checkInputBoxHasValue("description", description);
+	public void listAllNegative(final String url) {
+		super.navigate(url, "");
 		
 		super.checkErrorsExist();
-		super.signOut();
-
+	}
+	
+	@ParameterizedTest
+	@CsvFileSource(resources = "/authenticated/task/show-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@Order(30)
+	public void showNegative(final String url, final String query) {
+		super.navigate(url, query);
+		
+		super.checkErrorsExist();
 	}
 	
 }
